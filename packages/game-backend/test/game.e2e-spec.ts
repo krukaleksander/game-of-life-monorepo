3c916b4584a04e1f96984b83459750e8e1f8a596
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-20 14:00:15 +0200

    test: /api/game/tick (POST) response status 400 with id is a number

--- packages/game-backend/test/game.e2e-spec.ts
+++ packages/game-backend/test/game.e2e-spec.ts
@@ -27,4 +27,18 @@ describe('AppController (e2e)', () => {
       .send({})
       .expect(400);
   });
+  it('/api/game/tick (POST) response status 400 with id is a number', () => {
+    return request(app.getHttpServer())
+      .post('/game/tick')
+      .send({
+        "id": 1
+      })
+      .expect({
+        "statusCode": 400,
+        "message": [
+          "id must be a string"
+        ],
+        "error": "Bad Request"
+      });
+  });
 });
