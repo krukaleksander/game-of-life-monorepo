097c8b0d5aa891d7d3f55d831846ecde06265227
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-20 11:33:16 +0200

    feat: implement config for backend e2e tests

---
+++ packages/game-backend/jest-e2e.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/backend',
+  testMatch: ['./**/*.e2e-spec.ts']
+};
