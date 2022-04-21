fd4eacb5a859278112ec26fb78bc033b43bdabbd
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-21 15:50:00 +0200

    change anything to rebuild app 4

--- packages/game-front/service/endpoints.ts
+++ packages/game-front/service/endpoints.ts
@@ -1,4 +1,4 @@
 export const endpoints = {
-  SEND_INITIAL_BOARD: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/game`,
-  TICK: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/game/tick`,
+  SEND_INITIAL_BOARD: `${process.env.NEXT_PUBLIC_BACKEND_URL}api/game`,
+  TICK: `${process.env.NEXT_PUBLIC_BACKEND_URL}api/game/tick`,
 };
