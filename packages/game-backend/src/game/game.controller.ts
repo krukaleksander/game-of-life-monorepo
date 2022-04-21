34c3fe65b69480f6ba05a5272cef7cebed0a0ce6
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-21 09:10:45 +0200

    feat: game controller connect to the dto lib

--- packages/game-backend/src/game/game.controller.ts
+++ packages/game-backend/src/game/game.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GameService } from './game.service';
-import { BoardDto } from '../dto/board.dto';
-import { InitialResponseDto } from '../dto/initialResponse.dto';
-import { TickRequestDto } from '../dto/tickRequest.dto';
+import { GameDto } from '@gameoflife/game-of-life-dto';
+import { InitialResponseDto, TickRequestDto } from '@gameoflife/game-of-life-dto';
 
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Post('/')
-  setInitialBoard(@Body() board: BoardDto): InitialResponseDto {
+  setInitialBoard(@Body() board: GameDto): InitialResponseDto {
     return this.gameService.setInitialBoard(board);
   }
   @Post('/tick')
